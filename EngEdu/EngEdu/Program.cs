01457c985a3639679a2fafdedfe3ee40e153123f
muaywspkt <61030050@kmitl.ac.th>
2019-10-09 23:08:38 +0700

    2nd year semester1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -12,6 +12,7 @@ namespace EngEdu
         {
             Console.WriteLine("Year 255x semeter 1, I am an Engedu freshmen.");
             Console.WriteLine("Year 255x semester2, I learn to program in C sharp");
+            Console.WriteLine("Year 2560 semester1, I start my programmer life.");
 
 
         }
